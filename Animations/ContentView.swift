9497ecf36e806021f8604ec3b092076b3157fdfc
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-06-23 16:12:24 -0700

    Animating bindings

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,39 +11,26 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        VStack {
-            Button("Tap tap") {
-                //animationAmount += 1
+        print(animationAmount)
+        return VStack {
+            //Stepper("Scale amount", value: $animationAmount.animation(), in:1...10)
+            Stepper("Scale amount",
+                    value: $animationAmount.animation(
+                        .easeInOut(duration: 1)
+                        .repeatCount(3, autoreverses: true)
+                    ),
+                    in: 1...10
+            )
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
             }
-            .padding(50)
+            .padding(40)
             .background(.red)
             .foregroundStyle(.white)
             .clipShape(.circle)
-            //.scaleEffect(animationAmount)
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                        value: animationAmount
-                        
-                    )
-            )
-            //.blur(
-            //    radius: (animationAmount - 1) * 3
-            //)
-            .animation( //Implicit animation
-                .easeInOut(duration: 2),
-                //.repeatForever(autoreverses: true), //you can combine this with `onAppear()` to make this start immediately.
-                    //.delay(1)
-                    //.repeatCount(3, autoreverses: true), //Looks like # of movements, since 2 doesn't match the final state, it instantly jumps into the end state
-                value: animationAmount
-            )
-        }.onAppear {
-            animationAmount = 2
+            .scaleEffect(animationAmount)
         }
     }
 }
