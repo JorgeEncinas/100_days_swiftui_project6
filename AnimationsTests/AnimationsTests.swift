886cdb8c530c3ef8b2cce11b1e2fab30fe634baa
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-06-23 15:38:53 -0700

    First commit

---
+++ AnimationsTests/AnimationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsTests.swift
+//  AnimationsTests
+//
+//  Created by Jorge Encinas on 6/23/25.
+//
+
+import Testing
+@testable import Animations
+
+struct AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
